f17a83fe8cd546010c3a03735303d63693a2b140
Henry Cortez <Henry200amaya@gmail.com>
2021-05-28 01:14:05 -0600

    Debug: env files

---
+++ utils/parseError.js
@@ -0,0 +1,5 @@
+module.exports = (error) => {
+  const name = error?.details ? error.details[0].type.split('.')[1] : error.name
+  const message = error?.details ? error.details[0].message : error.message
+  return { name, message }
+}
